b6aef9f0e60906c8b8450955124fa7fa14c51168
Hilario Torres <hilario.c.torres@gmail.com>
2025-01-10 15:45:13 -0800

    Major clean up.

--- main.cpp
+++ main.cpp
@@ -68,30 +68,11 @@ int main (int argc, char* argv[])
     // DEFINE SIMULATION SETUP AND GEOMETRY
     // **********************************
 
-    // make BoxArray and Geometry
-    // ba will contain a list of boxes that cover the domain
-    // geom contains information such as the physical domain size,
-    // number of points in the domain, and periodicity
-
     // define lower and upper indices
     amrex::IntVect dom_lo(0,0);
     amrex::IntVect dom_hi(n_cell-1, n_cell-1);
-
-    // Make a single box that is the entire domain
-    //amrex::Box domain(dom_lo, dom_hi);
     
     amrex::Box cell_centered_box(dom_lo, dom_hi);
-    //amrex::Box x_face_centered_box(dom_lo, dom_hi+1, amrex::IndexType({1,0}));
-//    amrex::Box y_face_centered_box(dom_lo, dom_hi+1, amrex::IndexType({0,1}));
-//    amrex::Box node_centered_box = amrex::surroundingNodes(cell_centered_box);
-
-    //amrex::Box x_face_centered_box = amrex::convert(cell_centered_box, amrex::IndexType({1,0}));
-    //amrex::Box y_face_centered_box = amrex::convert(cell_centered_box, amrex::IndexType({0,1}));
-
-    //amrex::Print() << "Cell centered box " << cell_centered_box << std::endl;
-    //amrex::Print() << "X face centered box " << x_face_centered_box << std::endl;
-    //amrex::Print() << "y face centered box " << y_face_centered_box << std::endl;
-//    amrex::Print() << "node centered box " << node_centered_box << std::endl;
     
     amrex::Geometry geom;
     {
@@ -136,39 +117,20 @@ int main (int argc, char* argv[])
     amrex::DistributionMapping dm(ba);
     amrex::Print() << "dm " << dm << std::endl;
 
-    amrex::MultiFab psi_old(ba, dm, Ncomp, Nghost);
-    amrex::MultiFab psi_new(ba, dm, Ncomp, Nghost);
-
-
-    //// Set up face fields
-    //    // build the flux multifabs
-    //Array<MultiFab, AMREX_SPACEDIM> flux;
-    //for (int dir = 0; dir < AMREX_SPACEDIM; dir++)
-    //{
-    //    // flux(dir) has one component, zero ghost cells, and is nodal in direction dir
-    //    BoxArray edge_ba = ba;
-    //    edge_ba.surroundingNodes(dir);
-    //    flux[dir].define(edge_ba, dm, 1, 0);
-    //}
+    amrex::MultiFab psi(ba, dm, Ncomp, Nghost);
 
     amrex::BoxArray x_face_box_array = amrex::convert(ba, {1,0});
-    amrex::Print() << "x_face_box_array " << x_face_box_array << std::endl;
-    //amrex::MultiFab v(x_face_box_array, dm, Ncomp, 0);
+    //amrex::Print() << "x_face_box_array " << x_face_box_array << std::endl;
     amrex::MultiFab v(x_face_box_array, dm, Ncomp, Nghost); // Only used to compute h later... I think we can get around this by not compting h at all nodes
 
     amrex::BoxArray y_face_box_array = amrex::convert(ba, {0,1});
-    amrex::Print() << "y_face_box_array " << y_face_box_array << std::endl;
-    //amrex::MultiFab u(y_face_box_array, dm, Ncomp, 0);
+    //amrex::Print() << "y_face_box_array " << y_face_box_array << std::endl;
     amrex::MultiFab u(y_face_box_array, dm, Ncomp, Nghost);  // Only used to compute h later... I think we can get around this by not compting h at all nodes
 
     amrex::BoxArray surrounding_nodes_box_array= ba;
     surrounding_nodes_box_array.surroundingNodes();
-    amrex::Print() << "surrounding_nodes_box_array " << surrounding_nodes_box_array << std::endl;
+    //amrex::Print() << "surrounding_nodes_box_array " << surrounding_nodes_box_array << std::endl;
     amrex::MultiFab p(surrounding_nodes_box_array, dm, Ncomp, Nghost);
-
-    //BoxArray x_face_ba = ba;
-    //x_face_ba.surroundingNodes(0);
-    //amrex::MultiFab phi_new(ba, dm, Ncomp, Nghost);
 
 
     // **********************************
@@ -179,11 +141,11 @@ int main (int argc, char* argv[])
     amrex::Real a = 1000000;
 
     // loop over cell centers
-    for (amrex::MFIter mfi(psi_old); mfi.isValid(); ++mfi)
+    for (amrex::MFIter mfi(psi); mfi.isValid(); ++mfi)
     {
         const amrex::Box& bx = mfi.validbox();
 
-        const amrex::Array4<amrex::Real>& phiOld = psi_old.array(mfi);
+        const amrex::Array4<amrex::Real>& phi_array = psi.array(mfi);
 
         amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k)
         {
@@ -194,19 +156,14 @@ int main (int argc, char* argv[])
 
             amrex::Real x_cell_center = (i+0.5) * dx[0];
             amrex::Real y_cell_center = (j+0.5) * dx[1];
-            //amrex::Real x_node = (i) * dx[0];
-            //amrex::Real y_node = (j) * dx[1];
 
-            phiOld(i,j,k) = a*std::sin(x_cell_center)*std::sin(y_cell_center);
+            phi_array(i,j,k) = a*std::sin(x_cell_center)*std::sin(y_cell_center);
         });
     }
 
-    psi_old.FillBoundary(geom.periodicity());
+    psi.FillBoundary(geom.periodicity());
 
-    amrex::Print() << "psi_old min: " << psi_old.min(0) << std::endl;
-    amrex::Print() << "psi_old max: " << psi_old.max(0) << std::endl;
-
-    // coeffiecent for initialization of p
+    // loop over nodal points
     int N = n_cell; // Change to read into input file later... choose this name to correspond with the name from the python script
     double mesh_dx = 100000;
     double el = N*mesh_dx;
@@ -234,7 +191,7 @@ int main (int argc, char* argv[])
         const amrex::Box& bx = mfi.validbox();
 
         const amrex::Array4<amrex::Real>& u_array = u.array(mfi);
-        const amrex::Array4<amrex::Real>& phi_old_array = psi_old.array(mfi);
+        const amrex::Array4<amrex::Real>& phi_old_array = psi.array(mfi);
 
         amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k)
         {
@@ -248,7 +205,7 @@ int main (int argc, char* argv[])
         const amrex::Box& bx = mfi.validbox();
 
         const amrex::Array4<amrex::Real>& v_array = v.array(mfi);
-        const amrex::Array4<amrex::Real>& phi_old_array = psi_old.array(mfi);
+        const amrex::Array4<amrex::Real>& phi_old_array = psi.array(mfi);
 
         amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k)
         {
@@ -260,20 +217,22 @@ int main (int argc, char* argv[])
     u.FillBoundary(geom.periodicity());
     v.FillBoundary(geom.periodicity());
 
-    amrex::Print() << "p min: " << p.min(0) << std::endl;
+    amrex::Print() << "Initial: " << std::endl;
+    amrex::Print() << "psi max: " << psi.max(0) << std::endl;
+    amrex::Print() << "psi min: " << psi.min(0) << std::endl;
     amrex::Print() << "p max: " << p.max(0) << std::endl;
-    amrex::Print() << "u min: " << u.min(0) << std::endl;
+    amrex::Print() << "p min: " << p.min(0) << std::endl;
     amrex::Print() << "u max: " << u.max(0) << std::endl;
-    amrex::Print() << "v min: " << v.min(0) << std::endl;
+    amrex::Print() << "u min: " << u.min(0) << std::endl;
     amrex::Print() << "v max: " << v.max(0) << std::endl;
-
+    amrex::Print() << "v min: " << v.min(0) << std::endl;
 
     amrex::MultiFab output_values(ba, dm, 4, 0);
     for (amrex::MFIter mfi(output_values); mfi.isValid(); ++mfi)
     {
         const amrex::Box& bx = mfi.validbox();
 
-        const amrex::Array4<amrex::Real const>& phi_old_array = psi_old.const_array(mfi);
+        const amrex::Array4<amrex::Real const>& phi_old_array = psi.const_array(mfi);
         const amrex::Array4<amrex::Real const>& p_array = p.const_array(mfi);
         const amrex::Array4<amrex::Real const>& u_array = u.const_array(mfi);
         const amrex::Array4<amrex::Real const>& v_array = v.const_array(mfi);
@@ -295,35 +254,28 @@ int main (int argc, char* argv[])
 
     amrex::Real time = 0.0;
 
-    // Write a plotfile of the initial data if plot_int > 0
     if (plot_int > 0)
     {
-        int step = 0;
-        const std::string& pltfile = amrex::Concatenate("plt",step,5);
-        WriteSingleLevelPlotfile(pltfile, output_values, {"psi_old", "p", "u", "v"}, geom, time, 0);
+        int time_step = 0;
+        const std::string& pltfile = amrex::Concatenate("plt",time_step,5);
+        WriteSingleLevelPlotfile(pltfile, output_values, {"psi", "p", "u", "v"}, geom, time, 0);
     }
-
 
     // **********************************
     // MAIN TIME EVOLUTION LOOP
     // **********************************
 
-    // cu on the y faces
-    amrex::MultiFab cu(u.boxArray(), u.DistributionMap(), 1, Nghost);
+    // cu on the y faces (same locaions as u)
+    amrex::MultiFab cu(u.boxArray(), u.DistributionMap(), 1, u.nGrow());
 
-    // cv on the x faces
-    amrex::MultiFab cv(v.boxArray(), v.DistributionMap(), 1, Nghost);
+    // cv on the x faces (same locations as v)
+    amrex::MultiFab cv(v.boxArray(), v.DistributionMap(), 1, v.nGrow());
 
-    // z on the cell centers
-    amrex::MultiFab z(psi_old.boxArray(), psi_old.DistributionMap(), 1, Nghost);
+    // z on the cell centers (same locations as psi)
+    amrex::MultiFab z(psi.boxArray(), psi.DistributionMap(), 1, psi.nGrow());
 
-    // h on the nodal points
-    amrex::MultiFab h(p.boxArray(), p.DistributionMap(), 1, Nghost);
-
-    double fsdx = 4.0/mesh_dx;
-    double fsdy = 4.0/mesh_dy;
-
-    double tdt = dt;
+    // h on the nodal points (same locations as p)
+    amrex::MultiFab h(p.boxArray(), p.DistributionMap(), 1, p.nGrow());
 
     amrex::MultiFab u_old(u.boxArray(), u.DistributionMap(), u.nComp(), u.nGrow());
     amrex::MultiFab v_old(v.boxArray(), v.DistributionMap(), v.nComp(), v.nGrow());
@@ -333,15 +285,17 @@ int main (int argc, char* argv[])
     amrex::MultiFab v_new(v.boxArray(), v.DistributionMap(), v.nComp(), v.nGrow());
     amrex::MultiFab p_new(p.boxArray(), p.DistributionMap(), p.nComp(), p.nGrow());
 
-    amrex::MultiFab::Copy(u_old, u, 0, 0, u_old.nComp(), u_old.nGrow());
-    amrex::MultiFab::Copy(v_old, v, 0, 0, v_old.nComp(), v_old.nGrow());
-    amrex::MultiFab::Copy(p_old, p, 0, 0, p_old.nComp(), p_old.nGrow());
+    amrex::MultiFab::Copy(u_old, u, 0, 0, u.nComp(), u.nGrow());
+    amrex::MultiFab::Copy(v_old, v, 0, 0, v.nComp(), v.nGrow());
+    amrex::MultiFab::Copy(p_old, p, 0, 0, p.nComp(), p.nGrow());
 
+    double fsdx = 4.0/mesh_dx;
+    double fsdy = 4.0/mesh_dy;
+    double tdt = dt;
 
     for (int time_step = 0; time_step < nsteps; ++time_step)
     {
         // fill periodic ghost cells
-        psi_old.FillBoundary(geom.periodicity());
         u.FillBoundary(geom.periodicity());
         v.FillBoundary(geom.periodicity());
         p.FillBoundary(geom.periodicity());
@@ -459,66 +413,45 @@ int main (int argc, char* argv[])
 
         }
 
-//        // new_phi = old_phi + dt * Laplacian(old_phi)
-//        // loop over boxes
-//        for ( amrex::MFIter mfi(psi_old); mfi.isValid(); ++mfi )
-//        {
-//            const amrex::Box& bx = mfi.validbox();
-//
-//            const amrex::Array4<amrex::Real>& phiOld = psi_old.array(mfi);
-//            const amrex::Array4<amrex::Real>& phiNew = phi_new.array(mfi);
-//
-//            // advance the data by dt
-//            amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k)
-//            {
-//
-//                // **********************************
-//                // EVOLVE VALUES FOR EACH CELL
-//                // **********************************
-//
-//                //phiNew(i,j,k) = phiOld(i,j,k) + dt *
-//                //    ( (phiOld(i+1,j,k) - 2.*phiOld(i,j,k) + phiOld(i-1,j,k)) / (dx[0]*dx[0])
-//                //     +(phiOld(i,j+1,k) - 2.*phiOld(i,j,k) + phiOld(i,j-1,k)) / (dx[1]*dx[1])
-//                //     +(phiOld(i,j,k+1) - 2.*phiOld(i,j,k) + phiOld(i,j,k-1)) / (dx[2]*dx[2])
-//                //        );
-//
-//                phiNew(i,j,k) = phiOld(i,j,k) + dt *
-//                    ( (phiOld(i+1,j,k) - 2.*phiOld(i,j,k) + phiOld(i-1,j,k)) / (dx[0]*dx[0])
-//                     +(phiOld(i,j+1,k) - 2.*phiOld(i,j,k) + phiOld(i,j-1,k)) / (dx[1]*dx[1])
-//                        );
-//            });
-//        }
-//
-//        // **********************************
-//        // INCREMENT
-//        // **********************************
-//
-//        // update time
-//        time = time + dt;
-//
-//        // copy new solution into old solution
-//        amrex::MultiFab::Copy(psi_old, phi_new, 0, 0, 1, 0);
-//
-//        // Tell the I/O Processor to write out which step we're doing
-//        amrex::Print() << "Advanced step " << step << "\n";
-//
-//
-//        // **********************************
-//        // WRITE PLOTFILE AT GIVEN INTERVAL
-//        // **********************************
-//
-//        // Write a plotfile of the current data (plot_int was defined in the inputs file)
-//        if (plot_int > 0 && step%plot_int == 0)
-//        {
-//            const std::string& pltfile = amrex::Concatenate("plt",step,5);
-//            WriteSingleLevelPlotfile(pltfile, phi_new, {"phi"}, geom, time, step);
-//        }
+        // Write a plotfile of the current data (plot_int was defined in the inputs file)
+        if (plot_int > 0 && time_step%plot_int == 0)
+        {
+            for (amrex::MFIter mfi(output_values); mfi.isValid(); ++mfi)
+            {
+                const amrex::Box& bx = mfi.validbox();
+
+                const amrex::Array4<amrex::Real const>& phi_old_array = psi.const_array(mfi);
+                const amrex::Array4<amrex::Real const>& p_array = p.const_array(mfi);
+                const amrex::Array4<amrex::Real const>& u_array = u.const_array(mfi);
+                const amrex::Array4<amrex::Real const>& v_array = v.const_array(mfi);
+
+                const amrex::Array4<amrex::Real>& output_values_array = output_values.array(mfi);
+
+                amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k)
+                {
+                    output_values_array(i,j,k,0) = phi_old_array(i,j,k);
+                    output_values_array(i,j,k,1) = (p_array(i,j,k) + p_array(i+1,j,k) + p_array(i,j+1,k) + p_array(i+1,j+1,k))/4.0;
+                    output_values_array(i,j,k,2) = (u_array(i,j,k) + u_array(i,j+1,k))/2.0;
+                    output_values_array(i,j,k,3) = (v_array(i,j,k) + v_array(i+1,j,k))/2.0;
+                });
+            }
+
+            const std::string& pltfile = amrex::Concatenate("plt",time_step,5);
+            WriteSingleLevelPlotfile(pltfile, output_values, {"psi", "p", "u", "v"}, geom, time, time_step);
+        }
+
+        //amrex::Print() << "Done with time step " << time_step << std::endl;
     }
 
+    amrex::Print() << "Final: " << std::endl;
+    amrex::Print() << "p max: " << p.max(0) << std::endl;
+    amrex::Print() << "p min: " << p.min(0) << std::endl;
+    amrex::Print() << "u max: " << u.max(0) << std::endl;
+    amrex::Print() << "u min: " << u.min(0) << std::endl;
+    amrex::Print() << "v max: " << v.max(0) << std::endl;
+    amrex::Print() << "v min: " << v.min(0) << std::endl;
 
     }
     amrex::Finalize();
     return 0;
 }
-
-
--- plot.py
+++ plot.py
@@ -6,22 +6,25 @@ from yt.frontends.amrex.data_structures import AMReXDataset
 
 #yt.toggle_interactivity()
 
-ds = AMReXDataset("plt00000")
-#ds = AMReXDataset("plt01000")
+#ds = AMReXDataset("plt00000")
+#ds = AMReXDataset("plt03900")
 
-ds.field_list
+for filename in ["plt00000", "plt03900"]:
 
-sl_phi_old = yt.AxisAlignedSlicePlot(ds, 'z', ('boxlib', 'psi_old'), origin="native", axes_unit="unitary")
-sl_p = yt.AxisAlignedSlicePlot(ds, 'z', ('boxlib', 'p'), origin="native", axes_unit="unitary")
-sl_u = yt.AxisAlignedSlicePlot(ds, 'z', ('boxlib', 'u'), origin="native", axes_unit="unitary")
-#sl_u = yt.AxisAlignedSlicePlot(ds, 'z', ('boxlib', 'u'), origin="native")
-sl_v = yt.AxisAlignedSlicePlot(ds, 'z', ('boxlib', 'v'), origin="native", axes_unit="unitary")
-
-#sl_u.set_cmap(field=("boxlib", "u"), cmap="inferno")
-
-#sl.plots[("boxlib", "output_array")].axes.grid(True)
-
-sl_phi_old.save()
-sl_p.save()
-sl_u.save()
-sl_v.save()
+  ds = AMReXDataset(filename)
+  ds.field_list
+  
+  sl_phi_old = yt.AxisAlignedSlicePlot(ds, 'z', ('boxlib', 'psi'), origin="native", axes_unit="unitary")
+  sl_p = yt.AxisAlignedSlicePlot(ds, 'z', ('boxlib', 'p'), origin="native", axes_unit="unitary")
+  sl_u = yt.AxisAlignedSlicePlot(ds, 'z', ('boxlib', 'u'), origin="native", axes_unit="unitary")
+  #sl_u = yt.AxisAlignedSlicePlot(ds, 'z', ('boxlib', 'u'), origin="native")
+  sl_v = yt.AxisAlignedSlicePlot(ds, 'z', ('boxlib', 'v'), origin="native", axes_unit="unitary")
+  
+  #sl_u.set_cmap(field=("boxlib", "u"), cmap="inferno")
+  
+  #sl.plots[("boxlib", "output_array")].axes.grid(True)
+  
+  sl_phi_old.save()
+  sl_p.save()
+  sl_u.save()
+  sl_v.save()
